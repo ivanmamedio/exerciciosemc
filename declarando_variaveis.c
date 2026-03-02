fa2f3271ce95c7f392e88aa11100b41624eaf21a
Carlos Ivan Mamédio <carlos.ivan1997@outlook.com>
2026-03-01 22:29:22 -0300

    Add files via upload

---
+++ declarando_variaveis.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Declarando variáveis
+
+    int idade = 28;
+    float altura = 1.88;
+    char letra = 'A';
+
+    printf("Olá Mundo!\n");
+    printf("Idade: %d anos\n", idade);
+    printf("Altura: %.2f metros\n", altura);
+    printf("Letra: %c\n", letra);
+
+    return 0;
+}
