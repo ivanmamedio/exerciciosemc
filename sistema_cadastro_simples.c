97ac89f4eb4ed7ea5d2a7e584df97d89a810743b
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-03 23:34:22 -0300

    Código resolvido, algumas seções do código foram alteradas para correção dos erros

--- sistema_cadastro_simples.c
+++ sistema_cadastro_simples.c
@@ -25,19 +25,21 @@ int main() {
 
         printf("Idade: ");
         scanf("%d", &alunos[i].idade);
+        while (getchar() != '\n');  // limpa o buffer
 
         printf("Nota: ");
         scanf("%f", &alunos[i].nota);
+        while (getchar() != '\n');  // limpa para a próxima iteração
     }
 
     // Listagem 
     printf("\n=== LISTA DE ALUNOS ===\n");
     for(int i = 0; i < 3; i++) {
         printf("Aluno %d:\n", i+1);
-        printf(" Nome: %s\n", alunos[i].nome);
-        printf(" Idade: %d\n", alunos[i].idade);
-        printf(" Nota: %.2f\n", alunos[i].nota);
-        printf(" Status: %s\n", alunos[i].nota >= 7 ? "Aprovado" : "Reprovado");
+        printf("  Nome: %s\n", alunos[i].nome);
+        printf("  Idade: %d\n", alunos[i].idade);
+        printf("  Nota: %.2f\n", alunos[i].nota);
+        printf("  Status: %s\n", alunos[i].nota >= 7 ? "Aprovado" : "Reprovado");
     }
 
     return 0;
