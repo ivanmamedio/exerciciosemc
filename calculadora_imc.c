4a4330908d07ff4efd79071af4bb592bf2a124b9
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-11 20:57:55 -0300

    Tudo certo agora

--- calculadora_imc.c
+++ calculadora_imc.c
@@ -14,19 +14,18 @@ int main() {
     printf("Seu IMC é: %.2f\n", imc);
     printf("Classificação: ");
 
-    if (imc < 18.5) 
+    if (imc < 18.5)
         printf("Abaixo do peso\n");
     else if (imc < 25)
         printf("Peso normal\n");
     else if (imc < 30)
         printf("Sobrepeso\n");
     else if (imc < 35)
-        printf("Obesidade grau 1\n");
+        printf("Obesidade grau I\n");
     else if (imc < 40)
-        printf("Obesidade grau 2\n");
+        printf("Obesidade grau II\n");
     else
-        printf("Obesidade grau 3 (morbida)\n");
+        printf("Obesidade grau III (mórbida)\n");
 
     return 0;
-    
 }
