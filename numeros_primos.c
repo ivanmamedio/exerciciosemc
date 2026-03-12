7d7ab8bfb78ff4352f7901143fc2d2bbafa35378
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-11 21:09:51 -0300

    Resolvendo problemas de debug

--- numeros_primos.c
+++ numeros_primos.c
@@ -3,21 +3,21 @@
 
 // Função que verifica se um número é primo
 bool eh_primo(int n) {
-    if (n <= 1) return false;
+    if (n <= 2) return false;
     if (n == 2) return true;
     if (n % 2 == 0) return false;
 
-    for (int i = 3; i * i <+ n; i += 2) {
-        if (n % i == 0) return false;   
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
     }
     return true;
 }
 
 int main() {
-    printf("Números primos entre 1 e 100:\n");
+    printf("Números primos entre 2 e 100:\n");
 
     for (int i = 2; i <= 100; i++) {
-        if (e_primo(i)) {
+        if (eh_primo(i)) {
             printf("%d ", i);
         }
     }
