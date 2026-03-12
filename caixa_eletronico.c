07f72fa3661a6485150d17da0ccb8c9540b87fcb
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-11 21:51:58 -0300

    Finalizando e começando a testar

--- caixa_eletronico.c
+++ caixa_eletronico.c
@@ -15,7 +15,39 @@ int main() {
         scanf("%d", &opcao);
 
         switch("%d", &opcao) {
-            
+            case 1:
+                printf("Saldo atual: r$ %2.f\n"), saldo;
+                break;
+            case 2:
+                printf("Valor do depósito: R$ ");
+                scanf("%f", &valor);
+                if (valor > 0) {
+                    saldo += valor;
+                    printf("Depósito realizado. Novo saldo: R$ %2.f\n", saldo);
+                } else {
+                    printf("Valor inválido.\n");
+                }
+                break;
+            case 3:
+                printf("Valor do saque: R$ ");
+                scanf("%f", &valor);
+                if (valor > 0 && valor <= saldo) {
+                    saldo -= valor;
+                    printf("Saque realizado. Novo saldo: R$ %.2f\n", saldo);
+                } else if (valor > valor) {
+                    printf("Saldo insuficiente.\n");
+                } else {
+                    printf("Valor inválido.\n");
+                }
+                break;
+            case 4:
+                printf("Encerrando o sistema. Até logo!\n");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+
         }
-    }
+    }   while (opcao != 4);
+
+    return 0;
 }
