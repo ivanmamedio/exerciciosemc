9c300479c7975b6199c2dd845671ea00731d79f3
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-11 21:43:47 -0300

    Começando mais um. Algo mais complexo.

---
+++ caixa_eletronico.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float saldo = 1000.0; // saldo inicial
+    int opcao;
+    float valor;
+
+    do{
+        printf("\n=== Caixa Eletrônico ===\n");
+        printf("1 - Ver saldo\n");
+        printf("2 - Depositar\n");
+        printf("3 - Sacar\n");
+        printf("4 - Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch("%d", &opcao) {
+            
+        }
+    }
+}
